82e05bbd9fe4b8467274f651f0513af976c0c14f
sanjaySDET <sanjaybabusdet@gmail.com>
2023-11-30 12:53:57 +0530

    6c

---
+++ MyPjct/src/test/java/ptc/SampleTest.java
@@ -0,0 +1,17 @@
+package ptc;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	WebDriver driver;
+	
+	@Test
+	public void m1Test()
+	{
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.amazon.in");
+	}
+}
